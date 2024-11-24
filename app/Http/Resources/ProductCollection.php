82216e5f78fe5ecddde91d4a2d8a2cc48e978ceb
Tarique Mosharraf <applandsys@gmail.com>
2024-11-24 13:09:37 +0600

    feat: Product Resource and Collection created and document update

---
+++ app/Http/Resources/ProductCollection.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+                'current_page' => $this->currentPage(),
+                'product_data' => $this->collection->transform(function ($product) {
+                    return [
+                        'id' => $product->id,
+                        'name' => $product->name,
+                        'slug' => $product->slug,
+                        'price' => $product->price,
+                        'total_sales' => $product->total_sales,
+                        'reviews_avg_rating' => $product->reviews_avg_rating,
+                    ];
+                }),
+                'first_page_url' => $this->url(1),
+                'from' => $this->firstItem(),
+                'last_page' => $this->lastPage(),
+                'last_page_url' => $this->url($this->lastPage()),
+                'links' => $this->linkCollection(),
+                'next_page_url' => $this->nextPageUrl(),
+                'path' => $this->path(),
+                'per_page' => $this->perPage(),
+                'prev_page_url' => $this->previousPageUrl(),
+                'to' => $this->lastItem(),
+                'total' => $this->total(),
+        ];
+    }
+}
---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+
+    public function toArray($request)
+    {
+        //     return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'price' => $this->price,
+            'reviews_avg_rating' => $this->reviews_avg_rating,
+            'categories' => $this->categories,
+            'orderDetails' => $this->orderDetails,
+        ];
+    }
+}
